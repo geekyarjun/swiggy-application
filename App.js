5b0725da65556c955c58495d76203bb3d872389e
Arjun shrivastava <itsarjunshrivastava@gmail.com>
2020-07-02 05:23:28 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,125 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>SWIGGY</Text>
     </View>
   );
 }
 
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>SEARCH</Text>
+    </View>
+  );
+}
+
+function CartScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>CART</Text>
+    </View>
+  );
+}
+
+function AccountScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>ACCOUNT</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            return configueTabBarIcon(route, focused);
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="SWIGGY" component={HomeScreen} />
+        <Tab.Screen name="SEARCH" component={SettingsScreen} />
+        <Tab.Screen name="CART" component={CartScreen} />
+        <Tab.Screen name="ACCOUNT" component={AccountScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const configueTabBarIcon = (route, focused) => {
+  // You can return any component that you like here!
+  switch (route.name) {
+    case "SWIGGY": {
+      return (
+        <View>
+          <Image
+            source={
+              focused
+                ? require("./assets/swiggyVector.png")
+                : require("./assets/swiggy-outline.png")
+            }
+            style={styles.tabIcon}
+          />
+        </View>
+      );
+    }
+    case "SEARCH": {
+      return (
+        <FontAwesome5
+          name={"search"}
+          solid
+          style={{ color: focused ? "tomato" : "gray", fontSize: 15 }}
+        />
+      );
+    }
+    case "CART": {
+      return (
+        <FontAwesome5
+          name={"shopping-bag"}
+          solid
+          style={{ color: focused ? "tomato" : "gray", fontSize: 15 }}
+        />
+      );
+    }
+    case "ACCOUNT": {
+      return (
+        <FontAwesome5
+          name={"user"}
+          solid
+          style={{ color: focused ? "tomato" : "gray", fontSize: 15 }}
+        />
+      );
+    }
+  }
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tabIcon: {
+    resizeMode: "contain",
+    width: 20,
+    height: 20,
   },
 });
